7009d2287f4615820eaea53fd21180a8965eafd0
Deng Ming <ming_flycash@qq.com>
2024-02-11 23:19:47 +0800

    修复 github action 错误

--- .script/integrate_test.sh
+++ .script/integrate_test.sh
@@ -15,7 +15,7 @@
 #!/usr/bin/env bash
 
 set -e
-docker compose -f script/integration_test_compose.yml down
-docker compose -f script/integration_test_compose.yml up -d
+docker compose -f .script/integration_test_compose.yml down
+docker compose -f .script/integration_test_compose.yml up -d
 go test  ./...  -tags=e2e
-docker compose -f script/integration_test_compose.yml down
+docker compose -f .script/integration_test_compose.yml down
